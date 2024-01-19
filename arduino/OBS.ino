ac59584d0ed85544cce213b14981de187fa24923
Bilal Zakaria <77695101+BilalZakaria@users.noreply.github.com>
2024-01-18 18:02:23 -0600

    added comments to obs.ino

--- arduino/OBS.ino
+++ arduino/OBS.ino
@@ -20,59 +20,68 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 #define ECHO_PIN4 2
 #define TRIG_PIN4 3
 
-
-
-
 const byte switchPin = A5;
 
+// Create an array of ultrasonic sensors (SR04) with corresponding echo and trigger pins
+SR04 sonic[4] = {SR04(ECHO_PIN1, TRIG_PIN1), SR04(ECHO_PIN2, TRIG_PIN2), SR04(ECHO_PIN3, TRIG_PIN3), SR04(ECHO_PIN4, TRIG_PIN4)};
 
-SR04 sonic[4]= {SR04(ECHO_PIN1,TRIG_PIN1),SR04(ECHO_PIN2,TRIG_PIN2),SR04(ECHO_PIN3,TRIG_PIN3),SR04(ECHO_PIN4,TRIG_PIN4)};
+// Arrays to store distance measurements and motor intensities for each ultrasonic sensor
 float a[4];
 float intensity[4];
-float minrange=0;
-float maxrange=50;
-//int motor[]={2,3,4,5,6,7,8,9};
 
+// Define minimum and maximum range values for the sensors
+float minrange = 0;
+float maxrange = 50;
 
+// Initialize the setup function
 void setup() {
+  // Start serial communication at 9600 baud rate
   Serial.begin(9600);
+
+  // Initialize the Adafruit PWM servo driver
   pwm.begin();
-  //pwm.setOscillatorFrequency(27000000);
-  pwm.setPWMFreq(1600);//60
-  //Wire.setClock(400000);
-  //yield();
+
+  // Set the PWM frequency to 1600 Hz
+  pwm.setPWMFreq(1600);
+
+  // Set the switch pin as INPUT_PULLUP to enable internal pull-up resistor
   pinMode(switchPin, INPUT_PULLUP);
- 
 }
 
 void loop() {
-  byte switchState = LOW;//digitalRead(switchPin);  //read the state of the input pin (HIGH or LOW)
-  if (switchState == LOW)  //if it is LOW then the switch is closed
-  {
-    for(int i=0; i<4; i++){
-     a[i]=sonic[i].Distance();
-     if(a[i]>=minrange && a[i]<=maxrange){
-       intensity[i]=(1-((a[i]-minrange)/maxrange))*(4095);
-       //analogWrite(motor[i], inteffgnsity[i]); originally
-       pwm.setPWM(i,0,intensity[i]); 
-      Serial.print(i);
-      Serial.print(' ');
-      Serial.println(intensity[i]);
-       
-     }
-      else
-      {
-        pwm.setPWM(i,0,0); 
+  // Read the state of the switch (currently set to LOW for demonstration)
+  byte switchState = LOW; // digitalRead(switchPin); 
+
+  // Check if the switch is closed (LOW)
+  if (switchState == LOW) {
+    // Loop through each ultrasonic sensor
+    for (int i = 0; i < 4; i++) {
+      // Measure distance using the ultrasonic sensor
+      a[i] = sonic[i].Distance();
+
+      // Check if the measured distance is within the specified range
+      if (a[i] >= minrange && a[i] <= maxrange) {
+        // Calculate intensity based on distance within the range
+        intensity[i] = (1 - ((a[i] - minrange) / maxrange)) * 4095;
+
+        // Set PWM signal for the corresponding motor using Adafruit PWM servo driver
+        pwm.setPWM(i, 0, intensity[i]);
+
+        // Print sensor index and intensity for debugging
+        Serial.print(i);
+        Serial.print(' ');
+        Serial.println(intensity[i]);
+      } else {
+        // If distance is outside the range, turn off the corresponding motor
+        pwm.setPWM(i, 0, 0);
       }
     }
-  
-  } 
-  else
-  {
-    for(int i=0; i<4; i++){
-      pwm.setPWM(i,0,0); 
+  } else {
+    // If the switch is not closed, turn off all motors
+    for (int i = 0; i < 4; i++) {
+      pwm.setPWM(i, 0, 0);
     }
-    Serial.println("You weren't suppose to find this message");
-  }  
-  
+    Serial.println("You weren't supposed to find this message");
+  }
 }
+
